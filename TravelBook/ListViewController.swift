15dad771100765a60b6dd79ea4c2b32e4e4aae16
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-25 17:56:02 +0300

    user interface updated

---
+++ TravelBook/ListViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ListViewController.swift
+//  TravelBook
+//
+//  Created by Ayşegül Koçak on 25.02.2023.
+//
+
+import UIKit
+
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    @objc func addButtonClicked() {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+
+
+
+}
